4cb282d7db2a6968683fed6eb1220960e4234bd5
Matthew Moss <mdm@cse.unsw.edu.au>
2013-01-26 19:01:02 -0800

    Add file arguments and usage message

--- beta-texify.pl
+++ beta-texify.pl
@@ -2,18 +2,45 @@
 use strict;
 # Converts plain text into a beta friendly tex markup
 
-foreach my $line (<>){
+# Print usage if no arguments given
+die "Usage: $0 FILES...\n" if @ARGV == 0;
+
+foreach my $file (<*>){
+    # Skip any directories 
+    next if -d $file;
+
+    # Word out the rename
+    my $new_file = $file;
+    if ($file =~ /\..*$/){
+        $new_file =~ s/\..*$/\.tex/;
+    } else {
+        $new_file = $new_file.".tex";
+    }
+
+    # Skip any that already exist
+    next if -e $new_file;
+
+    open(INPUT, "<", $file);
+    open(OUTPUT, ">", $new_file);
+
+    for my $line (<INPUT>){
+        print OUTPUT translate_line($line);
+    }
+
+    close (INPUT);
+    close(OUTPUT)
+}
+
+sub translate_line ($){
+    my $line = $_[0];
+
     # Replace the underscores
     $line =~ s/_/\\_/g;
-
     # Add newline command to any double newlines
     $line =~ s/^$/\\newline\\newline\n/;
-
     # Now the fun part, apostrophes
     # If they occur after a letter, they probably are baktics
     $line =~ s/(\w)'/$1`/g;
 
-    print $line;
+    return $line;
 }
-
-
